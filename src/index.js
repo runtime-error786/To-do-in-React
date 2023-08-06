58ae61891dbab36e5b91b117214bd58b8e07b0e8
Mustafa Rizwan <mustafa782a@gmail.com>
2023-08-06 20:42:07 +0500

    all files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+let f;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+let count=0;
+function GH()
+{
+  
+let [list,set]=useState();
+let [r,set1] = useState([]);
+
+let Ji =()=>
+{
+  if(document.getElementById('lo').value!="")
+  {
+    document.getElementById('lo').value="";
+    set(f);
+    set1(()=>{
+      return ([...r,f]);
+    });
+  }
+  
+}
+
+let Hj =(event)=>
+{
+  f = event.target.value;
+}
+
+let Ob =(e)=>{
+  console.log("press delete button", e.target.id);
+  set1(r.filter((value,index)=>{
+     if(e.target.id!=index)   
+     {
+        return value; 
+     }
+  }));
+}
+
+  return(
+    <>
+    <div className='all'>
+    <div className='head'>
+      <h1 className='header'>TO DO LIST</h1>
+    </div>
+    <input type='text' id='lo' name='f1' placeholder='Add to do' className='i1' onChange={Hj}></input>
+    <button className='b1' onClick={Ji}>+</button>
+    {
+      r.map((value,index)=>{
+        return(
+          <>
+          <br></br>
+          <p className='j1'>{value}</p>
+          <button id={index} onClick={Ob} className='b2'>X</button>
+          </>
+        )
+      })
+    }
+    </div>
+    </>
+  )
+}
+ReactDOM.render(
+  <>
+<GH></GH>
+  </>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
